6d73b7a459839023b01b7a49ebd744f46cf7adf1
Vincent Parizet <vincent.parizet@hotmail.fr>
2019-08-24 21:57:21 +0200

    Add tinygltf in the repo

--- src/tiny_gltf.cpp
+++ src/tiny_gltf.cpp
@@ -2,5 +2,11 @@
 #define TINYGLTF_IMPLEMENTATION
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
+#define TINYGLTF_NO_INCLUDE_JSON
+#define TINYGLTF_NO_INCLUDE_STB_IMAGE
+#define TINYGLTF_NO_INCLUDE_STB_IMAGE_WRITE
 // # define TINYGLTF_NOEXCEPTION // optional. disable exception handling.
+#include <nlohmann/json.hpp>
+#include <stb_image.h>
+#include <stb_image_write.h>
 #include "tiny_gltf.h"
