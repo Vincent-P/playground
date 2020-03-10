fce4512668be082b54eaa821ede1b35134978773
Vincent Parizet <vincent.parizet@epita.fr>
2020-03-10 17:03:19 +0100

    add tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -14,3 +14,29 @@ TEST(ArenaTest, Size)
 
     EXPECT_EQ(numbers.get_size(), 0);
 }
+
+
+TEST(ArenaTest, ForLoop)
+{
+    Arena<uint> numbers;
+    auto h1 = numbers.add(1);
+
+    usize nb = 0;
+    for (auto number : numbers)
+    {
+        EXPECT_EQ(number, 1);
+        nb++;
+    }
+
+    EXPECT_EQ(nb, 1);
+
+    numbers.remove(h1);
+
+    nb = 0;
+    for (auto number : numbers)
+    {
+        nb++;
+    }
+
+    EXPECT_EQ(nb, 0);
+}
