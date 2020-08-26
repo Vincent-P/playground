31e5679f6da7ddca604167e3a5be0c2f2934f4a2
Vincent Parizet <vincent.parizet@hotmail.fr>
2020-08-26 20:42:46 +0200

    rengergraph thoughts

--- include/renderer/render_graph.hpp
+++ include/renderer/render_graph.hpp
@@ -1,4 +1,10 @@
 #pragma once
+#include "types.hpp"
+#include <vulkan/vulkan.h>
+#include <vector>
+#include <optional>
+#include <functional>
+
 /**
 
    Z prepass:
@@ -93,3 +99,91 @@
        "swapchain image"
 
  **/
+
+namespace my_app
+{
+namespace vulkan
+{
+struct API;
+};
+
+struct RenderPass;
+struct RenderGraph;
+
+enum struct SizeType
+{
+    Absolute,
+    SwapchainRelative
+};
+
+struct ImageDesc
+{
+    SizeType size_type;
+    float3 size;
+    VkImageType type;
+    VkFormat format;
+    uint samples;
+    uint levels;
+    uint layers;
+};
+using ImageDescH = Handle<ImageDesc>;
+
+enum struct PassType
+{
+    Graphics,
+    Compute
+    // Transfer?
+};
+
+
+using RenderPassH = Handle<RenderPass>;
+struct RenderPass
+{
+    PassType type;
+
+    std::function<void(RenderGraph&, vulkan::API&)> exec;
+
+    // params? (shader dynamic uniform buffer)
+
+    // inputs
+    std::vector<ImageDescH> sampled_images;
+    std::vector<ImageDescH> combined_sampler_images;
+    std::vector<ImageDescH> storage_images;
+
+    // outputs
+    std::vector<ImageDescH> color_attachments;
+    std::optional<ImageDescH> depth_attachment;
+};
+
+struct RenderGraph
+{
+    /**
+
+    how to specify external textures (e.g gltf textures?)
+
+    graph.add_pass({
+        .type = PassType::Graphics,
+        .sampled_images = { texture_a, texture_b, shadow_map },
+        .color_attachments = { hdr_buffer },
+        .depth = depth_buffer,
+        .exec = [this, data](RenderGraph& graph, API &api)
+        {
+            // ImageDesc -> vulkan::ImageH ?
+            draw_this();
+            draw_that();
+        }
+    });
+
+     **/
+    void add_pass(RenderPass&&);
+
+    Arena<RenderPassH> passes;
+
+    Arena<ImageDesc> color_attachments;
+    Arena<ImageDesc> sampled_images;
+    Arena<ImageDesc> combined_sampler_images;
+    Arena<ImageDesc> storage_images;
+    // or
+    Arena<ImageDesc> images;
+};
+} // namespace my_app
