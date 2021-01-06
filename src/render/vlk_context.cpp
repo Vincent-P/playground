b53ea35578cf1e2dd71ba1308ad563359485ce6a
Vincent Parizet <vincent.parizet@hotmail.fr>
2021-01-06 23:24:02 +0100

    fix warning

--- src/render/vlk_context.cpp
+++ src/render/vlk_context.cpp
@@ -1,11 +1,3 @@
-#if defined(_WIN32)
-#    define VK_USE_PLATFORM_WIN32_KHR
-#else
-#    define VK_USE_PLATFORM_XCB_KHR
-#endif
-
-#define VMA_IMPLEMENTATION
-
 #include "render/vlk_context.hpp"
 
 #include "platform/window.hpp"
