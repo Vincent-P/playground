8622f0326d9f4b994fa6c0b31be568cbd6e01830
Vincent Parizet <vincent.parizet@hotmail.fr>
2021-09-06 14:22:26 +0200

    exo: add EnumArray, an array that is indexed by an enum

---
+++ exo/include/exo/collections/enum_array.h
@@ -0,0 +1,65 @@
+#pragma once
+#include "exo/numerics.h"
+
+template <typename Enum>
+concept EnumCount = requires(Enum e)
+{
+    Enum::Count;
+};
+
+template <typename T, EnumCount E>
+struct EnumArray
+{
+    using Self                  = EnumArray<T, E>;
+    static constexpr usize SIZE = static_cast<usize>(E::Count);
+
+    constexpr const T &operator[](E e) const;
+    constexpr T &      operator[](E e);
+
+    constexpr const T *begin() const;
+    constexpr T *      begin();
+
+    constexpr const T *end() const;
+    constexpr T *      end();
+
+    // Note that the member data is intentionally public.
+    // This allows for aggregate initialization of the
+    // object (e.g. EnumArray<int, IntEnum> a = { 0, 3, 2, 4 }; )
+    T array[SIZE];
+};
+
+template <typename T, EnumCount E>
+constexpr const T &EnumArray<T, E>::operator[](E e) const
+{
+    return array[static_cast<usize>(e)];
+}
+
+template <typename T, EnumCount E>
+constexpr T &EnumArray<T, E>::operator[](E e)
+{
+    return const_cast<T &>(static_cast<const Self &>(*this)[e]);
+}
+
+template <typename T, EnumCount E>
+constexpr const T *EnumArray<T, E>::begin() const
+{
+    return &array[0];
+}
+
+template <typename T, EnumCount E>
+constexpr T *EnumArray<T, E>::begin()
+{
+    return const_cast<T *>(static_cast<const Self &>(*this).begin());
+}
+
+template <typename T, EnumCount E>
+constexpr const T *EnumArray<T, E>::end() const
+{
+    return &array[SIZE];
+}
+
+template <typename T, EnumCount E>
+constexpr T *EnumArray<T, E>::end()
+{
+    return const_cast<T *>(static_cast<const Self &>(*this).end());
+}
