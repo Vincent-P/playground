1f6e7d3ad6888d027afe9b36b98a05633f8fd7bf
Vincent Parizet <vincent.parizet@hotmail.fr>
2021-09-24 15:12:38 +0200

    engine: add pch to speed up compilation

---
+++ engine/src/pch.h
@@ -0,0 +1,22 @@
+#pragma once
+
+// std
+#include <chrono>
+#include <filesystem>
+#include <functional>
+#include <string>
+#include <vector>
+#include <span>
+#include <ranges>
+#include <algorithm>
+#include <array>
+
+// third party
+#include <fmt/core.h>
+
+#include <tracy/Tracy.hpp>
+
+// exo
+#include <exo/prelude.h>
+#include <exo/handle.h>
+#include <exo/collections/vector.h>
