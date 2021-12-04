94ac8edc9f761a9f61ab858616e8f2815cd434fb
Vincent Parizet <vincent.parizet@hotmail.fr>
2021-12-04 18:40:43 +0100

    exo/option: add helpers

--- exo/include/exo/base/option.h
+++ exo/include/exo/base/option.h
@@ -4,3 +4,17 @@
 
 template<typename T>
 using Option = std::optional<T>;
+
+template<typename T, typename ...Args>
+Option<T> Some(Args &&... args)
+{
+    return std::make_optional<T>(std::forward<Args>(args)...);
+}
+
+template<typename T>
+Option<T> Some(T && t)
+{
+    return Option<T>(std::forward<T>(t));
+}
+
+inline constexpr auto None = std::nullopt;
