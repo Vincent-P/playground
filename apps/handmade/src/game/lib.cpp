97646c3cd1e26e26c679b0709fee2db51e3a5400
Vincent Parizet <vincent.parizet@hotmail.fr>
2023-01-02 13:45:58 +0100

    [handmade] create window + vulkan context

--- apps/handmade/src/game/lib.cpp
+++ apps/handmade/src/game/lib.cpp
@@ -1,16 +1,53 @@
+#include "rhi/context.h"
+#include "rhi/device.h"
+#include "exo/logger.h"
 #include <cstdlib>
 
 struct GameState
 {
-	const char *awwhowhwowho;
-	char buf[1024];
 	int counter;
+	rhi::Context rhi_context;
+	rhi::Device rhi_device;
 };
 
 extern "C" __declspec(dllexport) GameState *init()
 {
 	auto *state = static_cast<GameState *>(calloc(1, sizeof(GameState)));
+
+	state->rhi_context = rhi::Context::create({.enable_validation = true});
+
+
+	auto &physical_devices = state->rhi_context.physical_devices;
+	u32 i_selected = u32_invalid;
+	u32 i_device = 0;
+	for (auto &physical_device : physical_devices) {
+		exo::logger::info("Found device: %s\n", physical_device.properties.deviceName);
+		if (i_device == u32_invalid && physical_device.properties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
+			exo::logger::info("Prioritizing device %s because it is a discrete GPU.\n",
+					  physical_device.properties.deviceName);
+			i_selected = i_device;
+		}
+		i_device += 1;
+	}
+	if (i_selected == u32_invalid) {
+		i_selected = 0;
+		exo::logger::info("No discrete GPU found, defaulting to device #0: %s.\n",
+				  physical_devices[0].properties.deviceName);
+	}
+
+	rhi::DeviceDescription device_desc = {};
+	device_desc.physical_device = &physical_devices[i_selected];
+
+	// Create the GPU
+	state->rhi_device = rhi::Device::create(state->rhi_context, device_desc);
+
 	return state;
+}
+
+extern "C" __declspec(dllexport) void reload(GameState *state)
+{
+	// do nothing
+	(void)(state);
 }
 
 extern "C" __declspec(dllexport) int update(GameState *state)
@@ -25,6 +62,9 @@ extern "C" __declspec(dllexport) int update(GameState *state)
 
 extern "C" __declspec(dllexport) void shutdown(GameState *state)
 {
-	// free
+	state->rhi_device.wait_idle();
+	state->rhi_device.destroy(state->rhi_context);
+	state->rhi_context.destroy();
+
 	free(state);
 }
--- apps/handmade/src/main.cpp
+++ apps/handmade/src/main.cpp
@@ -50,27 +50,101 @@ void unload_dynamic_module(DynamicModule *module)
 	module->shutdown_fn = nullptr;
 }
 
+struct Win32Window
+{
+	bool stop;
+};
+
+static LRESULT CALLBACK window_proc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	// Set or get the user dat aassociated to the window
+	Win32Window *window = nullptr;
+	if (uMsg == WM_CREATE) {
+		auto *p_create = reinterpret_cast<CREATESTRUCT *>(lParam);
+		window = static_cast<Win32Window *>(p_create->lpCreateParams);
+		SetWindowLongPtrW(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(window));
+	} else {
+		LONG_PTR const ptr = GetWindowLongPtrW(hwnd, GWLP_USERDATA);
+		window = reinterpret_cast<Win32Window *>(ptr);
+	}
+
+	switch (uMsg) {
+	case WM_CREATE: {
+		return 0;
+	}
+
+	case WM_CLOSE: {
+		window->stop = true;
+		break;
+	}
+
+	case WM_DESTROY: {
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	case WM_SETFOCUS: {
+		// window.has_focus = true;
+		return 0;
+	}
+	}
+
+	// default message handling
+	return DefWindowProcW(hwnd, uMsg, wParam, lParam);
+}
+
 int APIENTRY WinMain(HINSTANCE instance, HINSTANCE previous_instance, PSTR cmdline, int cmdshow)
 {
-	(void)(instance);
-	(void)(previous_instance);
-	(void)(cmdline);
-	(void)(cmdshow);
+	(void)(previous_instance); // Always NULL...
+	(void)(cmdline);           // Unused when UNICODE, use GetCommandLineW
 
+	// Create window
+	Win32Window win32_window = {};
+
+	// Register the window class
+	WNDCLASS wc = {};
+	wc.lpfnWndProc = window_proc;
+	wc.hInstance = instance;
+	wc.lpszClassName = L"Playground class";
+	wc.style = CS_OWNDC;
+	RegisterClassW(&wc);
+
+	// Create the window instance
+	auto hwnd = CreateWindowExW(WS_EX_TRANSPARENT, // Optional window styles.
+		wc.lpszClassName,                          // Window class
+		L"Playground",                             // Window text
+		WS_BORDER | WS_OVERLAPPEDWINDOW,           // Window style
+		// Position and size
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		// Width height
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		nullptr,      // Parent window
+		nullptr,      // Menu
+		instance,     // Instance handle
+		&win32_window // Additional application data
+	);
+
+	ShowWindow(hwnd, cmdshow);
+
+	// Load game DLL
 	const char *game_path = GAME_DLL_PATH;
 	const char *game_tmp_path = GAME_DLL_PATH ".tmp";
-
 	DynamicModule game = {};
-	char buffer[1024] = {};
-
 	load_dynamic_module(&game, game_path, game_tmp_path);
 	void *game_state = game.init_fn();
 
+	char buffer[1024] = {};
 	int value = 2;
-	for (;;) {
-		if (value == 0)
-			break;
+	MSG msg = {};
+	for (; !win32_window.stop;) {
+		if (PeekMessageW(&msg, nullptr, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessageW(&msg);
+		}
 
+		// Reload DLL
 		FILETIME game_module_date = win32_get_last_write_time(game_path);
 		bool is_game_up_to_date = game.latest_update.dwHighDateTime == game_module_date.dwHighDateTime &&
 		                          game.latest_update.dwLowDateTime == game_module_date.dwLowDateTime;
@@ -80,14 +154,14 @@ int APIENTRY WinMain(HINSTANCE instance, HINSTANCE previous_instance, PSTR cmdli
 			game.reload_fn(game_state);
 		}
 
-		// Write value on the debugger
+		// Update
 		auto written = snprintf(buffer, sizeof(buffer), "Value: %d\n", value);
 		buffer[written + 1] = 0;
 		OutputDebugStringA(buffer);
-
 		value = game.update_fn(game_state);
 	}
 	game.shutdown_fn(game_state);
+	unload_dynamic_module(&game);
 
-	return MessageBoxW(NULL, L"End.", L"The", 0);
+	return MessageBoxW(NULL, L"End.", L"The", MB_DEFAULT_DESKTOP_ONLY);
 }
